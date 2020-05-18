e2cd2076f84a7eef842aea44a42d5d81f93af9fe
Nicky Pochinkov <pochinkn@tcd.ie>
2020-05-18 15:35:04 +0100

    Altered menu SocialMediaLinks to only show links

--- src/components/SocialMediaLinks.js
+++ src/components/SocialMediaLinks.js
@@ -4,18 +4,22 @@ const config = require('../config.json')
 function SocialMediaLinks() {
   return(
   <div className="mainSocialMedia fadeIn">
+      { config.facebook &&
       <a href={config.facebook}> <span className="zoom fa-stack fa-lg socialMediaIcon">
         <i className="far fa-circle fa-stack-2x"></i>
         <i className="fab fa-facebook-f fa-stack-1x"></i>
       </span></a>
+      }{ config.instagram &&
       <a href={config.instagram}> <span className="zoom fa-stack fa-lg socialMediaIcon">
         <i className="far fa-circle fa-stack-2x"></i>
         <i className="fab fa-instagram fa-stack-1x"></i>
       </span></a>
+      }{ config.email &&
       <a href={"mailto:"+config.email}> <span className="zoom fa-stack fa-lg socialMediaIcon">
         <i className="far fa-circle fa-stack-2x"></i>
         <i className="fas fa-envelope fa-stack-1x"></i>
       </span> </a>
+      }
   </div>
   )
 }
