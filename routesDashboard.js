be5f75fa3953a289e20a81c3c82e041a9f6408af
Nicky Pochinkov <pochinkn@tcd.ie>
2020-05-25 00:11:33 +0100

    Added authenticated dashboard route

--- routesDashboard.js
+++ routesDashboard.js
@@ -1,5 +1,12 @@
 const passport = require('passport')
 
+const ensureAuthenticated = (req, res, next) => {
+  if ( req.isAuthenticated() ) {
+    return next()
+  }
+  res.redirect('/')
+}
+
 const routesDashboard = (app, db) => {
   // add pug as the view engine
   app.set('view-engine', 'pug')
@@ -10,12 +17,15 @@ const routesDashboard = (app, db) => {
   );
 
   // the callback after google has authorized the user
-  app.get('/auth/google/callback',
-    passport.authorize('google', {
-      successRedirect : '/dashboard/profile',
-      failureRedirect : '/login-failed'
-    }
-  ));
+  app.route('/auth/google/callback')
+    .get(passport.authorize('google', {failureRedirect : '/login-failed'}), (req, res) => {
+      res.redirect("/dashboard/profile")
+    });
+
+  app.route('/dashboard/profile')
+    .get(ensureAuthenticated, (req, res) => {
+      res.send("connected :)")
+    })
 }
 
 module.exports = routesDashboard;
