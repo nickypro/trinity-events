5c10d84cd0526b1ca6ab152aeadf72f1785c59c1
Nicky Pochinkov <pochinkn@tcd.ie>
2020-05-24 17:38:37 +0100

    moved sendEventData to separate api file

---
+++ apis/sendEventData.js
@@ -0,0 +1,35 @@
+const getEventsFromMySQL = require('../functions/getEventsFromMySQL')
+
+const sendEventData = async (req, res, connection, eventsFromToday, todayStringYMD, log = () => {}) => {
+  const startDate = (req.query.date && req.query.date.match(/\d{4}-\d{2}-\d{2}/)) ? req.query.date : todayStringYMD()
+  
+  let selected = []
+  if (req.query.socs) selected = await JSON.parse(req.query.socs)
+  if (selected.length == 0 | !Array.isArray(selected)) {
+    console.log(" - selected societies has length zero - sending for all societies")
+    log(" - selected societies has length zero - sending for all societies")
+    selected = []
+  }
+
+  if (startDate === todayStringYMD()) {
+    if (selected.length == 0) {
+      console.log(` - sending ${eventsFromToday.length} default events starting from today`)
+      log(` - sending ${eventsFromToday.length} default events starting from today`)
+      res.json(eventsFromToday);
+
+    } else {
+      console.log(` - sending filtered events starting from today`)
+      log(` - sending filtered events starting from today`)
+      selectedIds = new Set(selected)
+      res.json(eventsFromToday.filter( event => doIntersect(event.societyIds, selected) ))
+    }
+  } else {
+    console.log(` - performing manual database search for ${startDate}`)
+    log(` - performing manual database search for ${startDate}`)
+    getEventsFromMySQL(connection, startDate, (eventsRecieved) => {
+      res.json(eventsRecieved)
+    }, selected)
+  }
+}
+
+module.exports = sendEventData
