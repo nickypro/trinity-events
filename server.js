4f3f4180dd8e52d8bab5564a2738c15f1b663a37
Nicky Pochinkov <pochinkn@tcd.ie>
2020-05-25 18:34:34 +0100

    made .env have fixed path

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const session = require('express-session')
 const path = require('path');
 const app = express();
+require('dotenv').config({ path: path.resolve(__dirname, './.env') })
 
 const https = require('https');
 const fs = require('fs');
