08843a25c946bfdd9622953e242508c3af588822
Nicky Pochinkov <pochinkn@tcd.ie>
2021-03-07 00:37:44 +0000

    added Widgets to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './css/index.css';
+import Widgets from './Widgets'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Router>
+    <Switch>
+        <Route path="/widgets" component={Widgets} />
+        <Route path="/" component={App} />
+    </Switch>
+</Router>, 
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
