fde461d38611b00570a37d4184c2b94a24bd9b6c
Nicky Pochinkov <pochinkn@tcd.ie>
2021-03-07 02:17:09 +0000

    Added past events

--- src/components/EventsWidget.js
+++ src/components/EventsWidget.js
@@ -32,7 +32,7 @@ const EventsWidget = (props) => {
 
   return (
       <WidgetsWrapper >
-        
+
         <div className="events">
 
         {!socs && "Searching All Societies"}
@@ -59,6 +59,24 @@ const EventsWidget = (props) => {
             )
         }
 
+        {oldEvents && oldEvents.length != 0 && 
+        <div>
+          <h2 style={{textAlign: "center"}}>Past Events</h2>
+          {
+          oldEvents.reverse().map((event, index, arr) => (              
+            <div key={`event-div-${index}`}>
+                
+                {( !index || !timeFunc.datesAreOnSameDay( new Date(arr[index-1].date) , new Date(event.date) ) ) ?   
+                <h2 className="dateHeader grayed"> {dateFormat(event.date, "dddd dS mmmm yyyy")} </h2> 
+                : ""}
+
+                <Event event={event} key={event._id} className="grayed"/>
+                
+            </div>)
+          )
+          }
+        </div>
+        }
 
         </div>
 
