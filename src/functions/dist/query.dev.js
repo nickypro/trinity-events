b79959283f4abfb2a3da1abf0f7e77e126d484b6
Nicky Pochinkov <pochinkn@tcd.ie>
2021-03-07 01:05:28 +0000

    added getParameterByName to query.js

---
+++ src/functions/dist/query.dev.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+function getParameterByName(name) {
+  var url = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+var _default = getParameterByName;
+exports["default"] = _default;
